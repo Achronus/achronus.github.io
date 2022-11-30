6a6db0b8a5187282a93b3e0d6681f5605a0bbb43
Ryan Partridge <rpartridge101@yahoo.co.uk>
2022-11-30 19:42:57 +0000

    Removed js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
   setActiveClass();
-  setSinglePageNavClass();
   
   if (document.body.id != 'homepage') {
     // Create back to top button
@@ -87,12 +86,4 @@ function setActiveClass() {
       nav_links[i].classList.add('active');
     };
   };
-}
-
-function setSinglePageNavClass() {
-  valid_page = document.getElementById('top-page').classList.contains('single-page');
-  
-  if (valid_page) {
-    document.getElementById('site-navigation').classList.add('sticky-nav');
-  }
 }
